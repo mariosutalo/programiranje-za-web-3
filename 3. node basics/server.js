9544072574e8e880e0c62b37caa963721a3a3978
Mario Šutalo <mario.sutalo@gmail.com>
2025-11-26 11:38:12 +0100

    -cp

--- 3. node basics/server.js
+++ 3. node basics/server.js
@@ -1,12 +1,15 @@
 import http from "http"
+import lodash from "lodash"
+const { random } = lodash
 
 const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/plain')
     // res.write('Hi from node.js')
     // res.write(':)')
+    const randomNumber = random(1, 100)
     res.setHeader('Content-Type', 'text/html')
     res.write('<h5>Hi, this is heading</h5>')
-    res.write('<h5>Hi, this is heading</h5>')
+    res.write(`<p>Generated random number is: ${randomNumber}</p>`)
     res.end()
 })
 
