637aef7b9bde0b2a287b8caaa624c9273cf9bdba
Mario Šutalo <mario.sutalo@gmail.com>
2025-12-03 12:02:17 +0100

    -cp

--- 4. web shop/index.js
+++ 4. web shop/index.js
@@ -1,10 +1,28 @@
 import express from 'express'
 
 const app = express()
+app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res) => {
-    res.send('Hello from our first express page')
+    res.render('index', {productName: 'Gaming Pc'})
 })
+
+app.get('/products', (req, res) => {
+    res.render('products')
+})
+
+app.get('/blog', (req, res) => {
+    res.render('blog')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+
+
+
 
 app.listen(3000)
 
