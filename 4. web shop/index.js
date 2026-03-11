c046dd2c6f8d868172b5a9881d92276191dca5e7
Mario Šutalo <mario.sutalo@gmail.com>
2026-03-11 12:01:52 +0100

    -cp

--- 4. web shop/index.js
+++ 4. web shop/index.js
@@ -1,40 +1,46 @@
 import express from "express";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
-const dbConnection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "shop",
-});
-
-dbConnection.connect((error) => {
-  if (error) {
-    console.log("error connecting to db:", error);
-    return;
-  }
-});
+let dbConnection = null;
+try {
+  dbConnection = await mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "shop",
+  });
+} catch (error) {
+  console.log("error connecting to db", error);
+  process.exit();
+}
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  dbConnection.query(
-    `select id, name, price, stock
+app.get("/", async (req, res) => {
+  const productsPerPage = 10
+  const selectProductsQuery = `
+    select id, name, price, stock
     from product
-    limit 10 offset 0;`,
-    (error, results, fields) => {
-      if (error) {
-        console.log("error executing query", error);
-        res.render("server-error", {title: 'Server Error :('});
-        return;
-      }
-      console.log("simple select data", results);
-      res.render("index", { products: results, title: "Home Page" });
-    },
-  );
+    limit 10 offset 0;`;
+  const countProductsQuery = `
+    select count(*) as productsCount
+    from product;`;
+
+  try {
+    const [productsResults] = await dbConnection.query(selectProductsQuery);
+    const [countResults] = await dbConnection.query(countProductsQuery);
+    const productsCount = countResults[0].productsCount
+    const pagesCount = Math.ceil(productsCount/productsPerPage)
+    const currentPage = req.query.page
+
+    res.render("index", { products: productsResults, title: "Home Page" });
+  } catch (error) {
+    console.log("error executing query", error);
+    res.render("server-error", { title: "Server Error :(" });
+  }
 });
 
 app.get("/products", (req, res) => {
