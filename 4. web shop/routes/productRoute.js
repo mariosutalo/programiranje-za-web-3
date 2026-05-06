8789824c7f7ff0df31164893bfdeae616fb8c953
Mario Šutalo <mario.sutalo@gmail.com>
2026-05-06 12:20:25 +0200

    -cp

--- 4. web shop/routes/productRoute.js
+++ 4. web shop/routes/productRoute.js
@@ -2,19 +2,33 @@ import express from "express";
 // ./ - trenutna mapa, ../ - mapa iznad
 import { dbConnection } from "../index.js";
 import { appConstants } from "../config/appConstants.js";
+import * as z from "zod";
 
 export const router = express.Router();
+
+const productIdSchema = z.number().int().positive();
 
 router.get("/", (req, res) => {
   res.render("products");
 });
 
-router.get("/details", (req, res) => {
+router.get("/details", async (req, res) => {
   const productId = req.query.id;
   const productIdAsNumber = Number(productId);
-  if (Number.isInteger(productIdAsNumber) === false || productIdAsNumber < 1) {
+  const productIdResult = productIdSchema.safeParse(productIdAsNumber);
+  if (productIdResult.error) {
     res.render("server-error");
-    return
+    return;
   }
-  res.render("product-details", { title: "Product Details" });
+  const productDetailsQuery = `
+  select *
+  from product
+  where id=${productIdAsNumber};
+  `;
+  const [productDetailsResult] = await dbConnection.query(productDetailsQuery);
+
+  res.render("product-details", {
+    title: "Product Details",
+    productDetails: productDetailsResult[0],
+  });
 });
